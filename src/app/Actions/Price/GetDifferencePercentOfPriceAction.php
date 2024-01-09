35ab5be43542567358a665fa8b30aaadb38cb7b0
gnkit <hope_g@mail.ru>
2024-01-09 10:00:00 +0000

    refactored difference price

--- src/app/Actions/Price/GetDifferencePercentOfPriceAction.php
+++ src/app/Actions/Price/GetDifferencePercentOfPriceAction.php
@@ -6,12 +6,6 @@ final class GetDifferencePercentOfPriceAction
 {
     public static function execute($currentPrice, $newPrice): float
     {
-        if ($currentPrice > $newPrice) {
-
-            return round(($currentPrice / ($currentPrice - $newPrice)) / 100, 2);
-        } else {
-
-            return round(($newPrice / ($newPrice - $currentPrice)) / 100, 2);
-        }
+        return round(((abs($currentPrice - $newPrice)) / $currentPrice) * 100, 2);
     }
 }
